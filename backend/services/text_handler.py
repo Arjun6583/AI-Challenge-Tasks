41fc0921683f88b1405c365b62787f5e61e5c57c
Arjun Namdev Ghule <150027336+Arjun6583@users.noreply.github.com>
2025-09-23 13:43:47 +0530

    Update text_handler.py
    text_handler added language detect code added

--- backend/services/text_handler.py
+++ backend/services/text_handler.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from utils.helpers import ensure_dir, unique_stem
 from utils.hash_utils import calculate_md5
 from database.db import collection
-from zoneinfo import ZoneInfo
+from zoneinfo import ZoneInfo 
+import services.llm_service as llm_service
 
 def save_text_block(text: str, target_dir: Path, ext: str = ".txt") -> dict:
     try:
@@ -15,20 +16,28 @@ def save_text_block(text: str, target_dir: Path, ext: str = ".txt") -> dict:
         with dest.open("w", encoding="utf-8") as f:
             f.write(text)
 
+        content = text
+        #call api to get file type 
+        code_blocks = llm_service.llm_guard_code_object._extract_code_blocks(content)
+        code_blocks = [content]
+        results = llm_service.llm_guard_code_object._pipeline(code_blocks)
+        language_detect = results[0][0]['label'] 
+        
         # calculate MD5 checksum
         file_md5 = calculate_md5(dest)
         metadata = {
             "path": str(dest.resolve()),
             "current_time": datetime.now(ZoneInfo("Asia/Kolkata")).isoformat(),
             "md5": file_md5,
-            "filetype": "text"
+            "filetype": "text",
+            "language": language_detect
         }
 
         collection.insert_one(metadata)
 
         # prepare response
         response = {
-            "ai_response": "Give Text code is clean",
+            "ai_response": f"Language Detect {language_detect}",
             "file_path": str(dest),
             "message": "Text block saved and metadata stored",
             "analysis": f"This is latest analysis from LLM {text[:30]}"
